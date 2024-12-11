d4911ed22184f127851cf694d28e810eb303e457
rufezzz <83346207+rufezzz@users.noreply.github.com>
2024-12-11 18:19:50 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
@@ -11,9 +10,66 @@ public class Main {
         System.out.println("Два средних знака строки 'Practice': " + getMiddleCharacters("Practice"));
 
 
-        System.out.println("\n C) Слово с минимальным числом различных символов: " + findWordWithMinUniqueChars("fda ab fa aaaaaaaaa 1234 jkjk"));
+        System.out.println("\nC) Слово с минимальным числом различных символов: " + findWordWithMinUniqueChars("fda ab fa aaaaaaaaa 1234 jkjk"));
 
 
         System.out.println("\n D) Количество слов, содержащих только символы латинского алфавита: " + countLatinWords("One two three раз два три one1 two2 123"));
+    }
+
+
+    public static void printFilteredNumbers() {
+        for (int i = 0; i <= 300; i++) {
+            if (i % 4 == 0 && i % 6 != 0 && sumOfDigits(i) < 10) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+
+    private static int sumOfDigits(int number) {
+        int sum = 0;
+        while (number > 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+
+
+    public static String getMiddleCharacters(String str) {
+        int length = str.length();
+        if (length % 2 != 0) {
+            throw new IllegalArgumentException("Длина строки должна быть четной.");
+        }
+        int midIndex = length / 2;
+        return str.substring(midIndex - 1, midIndex + 1);
+    }
+
+
+    public static String findWordWithMinUniqueChars(String str) {
+        String[] words = str.split(" ");
+        String minWord = "";
+        int minUniqueCount = Integer.MAX_VALUE;
+
+        for (String word : words) {
+            int uniqueCount = (int) word.chars().distinct().count();
+            if (uniqueCount < minUniqueCount) {
+                minUniqueCount = uniqueCount;
+                minWord = word;
+            }
+        }
+        return minWord;
+    }
+
+    public static int countLatinWords(String str) {
+        String[] words = str.split(" ");
+        int count = 0;
+
+        for (String word : words) {
+            if (word.matches("[a-zA-Z]+")) {
+                count++;
+            }
+        }
+        return count;
     }
 }
